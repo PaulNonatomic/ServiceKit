3c452a5ce4a60c07d186c537ff0e9afabbc3500d
Paul Stamp <paul@nonatomic.co.uk>
2025-05-26 09:21:49 +0100

    Version 2 implementation

---
+++ Runtime/IServiceInjectionBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nonatomic.ServiceKit
+{
+	public interface IServiceInjectionBuilder
+	{
+		IServiceInjectionBuilder WithCancellation(CancellationToken cancellationToken);
+		IServiceInjectionBuilder WithTimeout(float timeoutSeconds);
+		IServiceInjectionBuilder WithTimeout();
+		IServiceInjectionBuilder WithErrorHandling(Action<Exception> errorHandler);
+		IServiceInjectionBuilder WithErrorHandling();
+
+		void Execute();
+		Task ExecuteAsync();
+	}
+}
---
+++ Runtime/ServiceKitBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Nonatomic.ServiceKit
+{
+	public abstract class ServiceKitBehaviour<T> : MonoBehaviour where T : class 
+	{
+		[SerializeField] protected ServiceKitLocator _serviceKitLocator;
+
+		protected async virtual void Awake()
+		{
+			await InjectServices();
+		}
+
+		protected virtual void RegisterService()
+		{
+			if (GuardAgainstUnassignedServiceKit()) return;
+			
+			_serviceKitLocator.RegisterService<T>(this as T);
+		}
+
+		protected async virtual Task InjectServices()
+		{
+			if (GuardAgainstUnassignedServiceKit()) return;
+			
+			await _serviceKitLocator.InjectServicesAsync(this)
+				.WithCancellation(destroyCancellationToken) 
+				.WithTimeout()
+				.WithErrorHandling()
+				.ExecuteAsync();
+		}
+		
+		protected bool GuardAgainstUnassignedServiceKit()
+		{
+			if (_serviceKitLocator) return false;
+			
+			Debug.LogError($"{GetType().Name} requires a reference to a ServiceKitLocator.", this);
+			return true;
+		}
+	}
+}
