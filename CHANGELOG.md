458bb416339756fd1eebfce8995a5473d08e12aa
Paul Stamp <paul@nonatomic.co.uk>
2025-06-26 12:21:43 +0100

    Removed use of ScriptableInstance in runtime code

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## [0.1.2] - Jun 26, 2025
+- Removed use of ScriptableSingleton in Settings because it's an Editor only class
--- Runtime/ServiceKitSettings.cs
+++ Runtime/ServiceKitSettings.cs
@@ -1,5 +1,4 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Nonatomic.ServiceKit
 {
@@ -7,10 +6,15 @@ namespace Nonatomic.ServiceKit
 	/// Runtime settings for ServiceKit
 	/// </summary>
 	[CreateAssetMenu(fileName = "ServiceKitSettings", menuName = "ServiceKit/Settings")]
-	public class ServiceKitSettings : ScriptableSingleton<ServiceKitSettings>
+	public class ServiceKitSettings : ScriptableObject
 	{
+		public bool AutoCleanupOnSceneUnload => _autoCleanupOnSceneUnload;
+		public bool DebugLogging => _debugLogging;
+		public bool WarnOnDestroyedRegistration => _warnOnDestroyedRegistration;
+		public float DefaultTimeout => _defaultTimeout;
+		
 		private static ServiceKitSettings _instance;
-
+		
 		[SerializeField]
 		[Tooltip("Automatically unregister MonoBehaviour services when their scene is unloaded")]
 		private bool _autoCleanupOnSceneUnload = true;
@@ -26,40 +30,30 @@ namespace Nonatomic.ServiceKit
 		[SerializeField]
 		[Tooltip("Maximum time to wait for services before timing out (in seconds)")]
 		private float _defaultTimeout = 30f;
+	
+		public static ServiceKitSettings Instance
+		{
+			get
+			{
+				if (_instance != null) return _instance;
+				
+				_instance = Resources.Load<ServiceKitSettings>("ServiceKitSettings");
+				if (_instance != null) return _instance;
 
-		public bool AutoCleanupOnSceneUnload
-		{
-			get
-			{
-				return _autoCleanupOnSceneUnload;
+				_instance = CreateInstance<ServiceKitSettings>();
+				_instance.name = "ServiceKitSettings (Runtime)";
+                        
+				#if UNITY_EDITOR
+				Debug.LogWarning("ServiceKitSettings: No asset found in Resources folder. Using runtime instance. " +
+								 "Consider creating a ServiceKitSettings asset in a Resources folder for persistent settings.");
+				#endif
+				return _instance;
 			}
 		}
-		public bool DebugLogging
-		{
-			get
-			{
-				return _debugLogging;
-			}
-		}
-		public bool WarnOnDestroyedRegistration
-		{
-			get
-			{
-				return _warnOnDestroyedRegistration;
-			}
-		}
-		public float DefaultTimeout
-		{
-			get
-			{
-				return _defaultTimeout;
-			}
-		}
-
+		
 		#if UNITY_EDITOR
 		private void OnValidate()
 		{
-			//Ensure the timeout is always positive
 			_defaultTimeout = Mathf.Max(0.1f, _defaultTimeout);
 		}
 		#endif
